7361389ac4d70d2529cff9d83f78eaffefb49ffa
DDavidescu <dbau43@yahoo.com>
2025-07-01 18:44:59 +0300

    Deleted .js files updated .gitignore

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Directly hard-code your URL and anon key here:
 const supabaseUrl = 'https://kekluxmzniujyrddhro.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtla2xrdXhtem5pdWp5cmRkaHJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1NTA1NDgsImV4cCI6MjA2NDEyNjU0OH0.N5Y4J8RkKQzX0v4QW7dXaoO4N0_lhatYBIXGiV98XP4'
 
