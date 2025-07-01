aed27c0c4510b5e31e3d7d7aec4dc88a9e9aab93
DDavidescu <150448912+DDavidescu@users.noreply.github.com>
2025-07-01 17:47:43 +0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -32,7 +32,7 @@ export default function Home() {
     const [showAuth, setShowAuth] = useState(false);
     const [showGameModeModal, setShowGameModeModal] = useState(false);
     const predefinedAvatars = [
-        "/src/assets/smiling_globe_profilePicture.png",
+        "src/assets/smiling_globe_profilePicture.png",
         "src/assets/compass_profilePicture.png",
         "src/assets/airplane_profilePicture.png",
         "src/assets/cactus_profilePicture.png",
