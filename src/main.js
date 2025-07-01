010dd90fb101184278d6d0870198e7d11174a45d
DDavidescu <dbau43@yahoo.com>
2025-07-01 18:28:14 +0300

    Updates

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { UserProvider } from './context/UserContext';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './index.css';
-ReactDOM.createRoot(document.getElementById('root')).render(_jsx(React.StrictMode, { children: _jsx(BrowserRouter, { basename: import.meta.env.BASE_URL, children: _jsx(UserProvider, { children: _jsx(App, {}) }) }) }));
