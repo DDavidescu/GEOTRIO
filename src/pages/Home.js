3faa19cd80a3e5f882df3b646f0cd84128167888
DDavidescu <dbau43@yahoo.com>
2025-07-01 18:41:36 +0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -32,12 +32,12 @@ export default function Home() {
     const [showAuth, setShowAuth] = useState(false);
     const [showGameModeModal, setShowGameModeModal] = useState(false);
     const predefinedAvatars = [
-        "src/assets/smiling_globe_profilePicture.png",
-        "src/assets/compass_profilePicture.png",
-        "src/assets/airplane_profilePicture.png",
-        "src/assets/cactus_profilePicture.png",
-        "src/assets/statue_profilePicture.png",
-        "src/assets/oldmap_profilePicture.png"
+        "/assets/smiling_globe_profilePicture.png",
+        "/assets/compass_profilePicture.png",
+        "/assets/airplane_profilePicture.png",
+        "/assets/cactus_profilePicture.png",
+        "/assets/statue_profilePicture.png",
+        "/assets/oldmap_profilePicture.png"
     ];
     useEffect(() => {
         const fetchTopScores = async () => {
