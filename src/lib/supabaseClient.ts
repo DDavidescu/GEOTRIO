f06c6ecdfedaa908ff8284b5046e9d8043a9824d
DDavidescu <dbau43@yahoo.com>
2025-07-01 18:47:38 +0300

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://kekluxmzniujyrddhro.supabase.co'
+const supabaseUrl = 'https://keklkuxmzniujyrddhro.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtla2xrdXhtem5pdWp5cmRkaHJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1NTA1NDgsImV4cCI6MjA2NDEyNjU0OH0.N5Y4J8RkKQzX0v4QW7dXaoO4N0_lhatYBIXGiV98XP4'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
